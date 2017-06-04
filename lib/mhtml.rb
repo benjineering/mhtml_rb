69149ccf7e90d7a3ca2e20aea3f84391ca650079
Ben Williams <8enwilliams@gmail.com>
2017-06-04 19:36:55 +1000

    fat trim

--- lib/mhtml.rb
+++ lib/mhtml.rb
@@ -1,9 +1,6 @@
-require 'mhtml/version'
-
 require 'mhtml/document'
-require 'mhtml/header'
 require 'mhtml/http_header'
-require 'mhtml/sub_document'
+require 'mhtml/version'
 
 module Mhtml
   LINE_BREAK = "\r\n"
