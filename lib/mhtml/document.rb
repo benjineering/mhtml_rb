63823cf975c6a85cbe44085b22a744848613eb8e
Ben Williams <8enwilliams@gmail.com>
2017-08-24 16:27:47 +1000

    super class working sans subdoc

--- lib/mhtml/document.rb
+++ lib/mhtml/document.rb
@@ -2,32 +2,44 @@ require 'http-parser'
 
 module Mhtml
   class Document
-    attr_accessor :headers, :body
+    attr_accessor :headers, :body, :chunked
 
     def initialize(str_or_headers_proc, body_proc = nil)
-      if str_or_headers_proc.is_a?(String)
-        @headers = []
+      @chunked = !str_or_headers_proc.is_a?(String)
+      @header_key = nil
+      @header_value_lines = nil
+      @body_read = false
 
-        str = str_or_headers_proc
-        body_pos = str.index(Mhtml::DOUBLE_LINE_BREAK)
+      @request = HttpParser::Parser.new_instance { |inst| inst.type = :response }
 
-        header_str = str[0..body_pos].strip
-        header_str.gsub!(/\r?\n\s+/, " ") # handle multiline headers
-
-        header_str.split(Mhtml::LINE_BREAK).each do |line|
-          @headers << HttpHeader.new(line)       
+      @parser = HttpParser::Parser.new do |parser|
+        parser.on_header_field do |inst, data|
+          handle_header_field(inst, data)
         end
 
-        @body = str[(body_pos + 1)..(str.length - 1)]
+        parser.on_header_value do |inst, data|
+          handle_header_value(inst, data)
+        end
 
-      else
+        parser.on_body do |inst, data|
+          handle_body(inst, data)
+        end
+      end
+
+      @parser.parse(@request, Mhtml::STATUS_LINE)
+
+      if @chunked
         @headers_proc = str_or_headers_proc
         @body_proc = body_proc
+      else
+        @headers = []
+        @body = ''
+        @parser.parse(@request, str_or_headers_proc)
       end
     end
 
     def <<(chunk)
-      
+      @parser.parse(@request, chunk)
     end
 
     def ==(other)
@@ -37,6 +49,53 @@ module Mhtml
     # for testing only = no spec implemented
     def to_s
       @headers.join(LINE_BREAK) + Mhtml::DOUBLE_LINE_BREAK + @body
+    end
+
+    private
+
+    def handle_header_field(inst, data)
+      maybe_create_header
+      @header_key = data
+      @header_value_lines = []
+    end
+
+    def handle_header_value(inst, data)
+      @header_value_lines << data
+    end
+
+    def handle_body(inst, data)
+      parts = data.split(boundary_str)
+
+      unless @body_read
+        maybe_create_header
+
+        if @chunked
+          @body_proc.call(parts.first) unless @body_proc.nil?
+        else
+          @body += parts.first
+        end
+
+        @body_read = data.include?(Mhtml::LINE_BREAK)
+      end
+
+      return parts
+    end
+
+    def maybe_create_header
+      unless @header_key.nil?
+        header = HttpHeader.new(@header_key, @header_value_lines)
+        @headers << header unless @chunked
+
+        if header.key == 'Content-Type'
+          b_header = header.values.select { |v| v.key == 'boundary' }.first
+          @boundary = b_header.value unless b_header.nil?
+        end
+
+        @headers_proc.call(header) unless @headers_proc.nil?
+
+        @header_key = nil
+        @header_value_lines = []
+      end
     end
   end
 end
--- lib/mhtml/root_document.rb
+++ lib/mhtml/root_document.rb
@@ -6,59 +6,15 @@ module Mhtml
     BOUNDARY_PREFIX = '--'.freeze
 
     def initialize(str_or_headers_proc, body_proc = nil, subdocs_proc = nil)
-      @chunked = !str_or_headers_proc.is_a?(String)
-      @header_key = nil
-      @header_value_lines = nil
-      @body_read = false
-
-      @request = HttpParser::Parser.new_instance { |inst| inst.type = :response }
-
-      @parser = HttpParser::Parser.new do |parser|
-        parser.on_header_field do |inst, data|
-          maybe_create_header
-          @header_key = data
-          @header_value_lines = []
-        end
-
-        parser.on_header_value do |inst, data|
-          @header_value_lines << data
-        end
-
-        parser.on_body do |inst, data|
-          unless @body_read
-            maybe_create_header
-            parts = data.split(boundary_str)
-
-            if @chunked
-              @body_proc.call(parts.first) unless @body_proc.nil?
-            else
-              @body += parts.first
-            end
-
-            @body_read = data.include?(Mhtml::LINE_BREAK)
-          end
-
-          # TODO: handle subdocs
-        end
-      end
-
-      @parser.parse(@request, Mhtml::STATUS_LINE)
-
       if @chunked
-        @headers_proc = str_or_headers_proc
-        @body_proc = body_proc
-        @subdocs_proc = subdocs_proc
+        @subdocs_proc = subdocs_proc 
       else
-        @headers = []
         @subdocs = []
-        @body = ''
-
-        @parser.parse(@request, str_or_headers_proc)
       end
-    end
 
-    def <<(chunk)
-      @parser.parse(@request, chunk)
+      @subdoc = nil
+
+      super(str_or_headers_proc, body_proc)
     end
 
     def ==(other)
@@ -70,6 +26,17 @@ module Mhtml
 #{Mhtml::LINE_BREAK}"
     end
 
+    def handle_body(inst, data)
+      parts = super(inst, data)
+
+      # TODO: handle subdocs
+      if @body_read || parts.length > 1
+        if @subdoc.nil?
+          @subdoc = Document.new()
+        end
+      end
+    end
+
     # for testing only = no spec implemented
     def to_s
       doc_sep = Mhtml::DOUBLE_LINE_BREAK + BOUNDARY_PREFIX + @boundary + 
@@ -79,21 +46,16 @@ module Mhtml
 
     private
 
-    def maybe_create_header
-      unless @header_key.nil?
-        header = HttpHeader.new(@header_key, @header_value_lines)
-        @headers << header unless @chunked
+    def handle_subdoc_header(header)
+      if @chunked
+        
+      else
 
-        if header.key == 'Content-Type'
-          b_header = header.values.select { |v| v.key == 'boundary' }.first
-          @boundary = b_header.value unless b_header.nil?
-        end
-
-        @headers_proc.call(header) unless @headers_proc.nil?
-
-        @header_key = nil
-        @header_value_lines = []
       end
+    end
+
+    def handle_subdoc_body(str)
+
     end
   end
 end
