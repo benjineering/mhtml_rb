34c851f386281a5609e76e5a4277bcb1b33a6af3
Ben Williams <8enwilliams@gmail.com>
2017-09-18 12:49:51 +1000

    .is_mhtml method

--- lib/mhtml.rb
+++ lib/mhtml.rb
@@ -1,8 +1,19 @@
 
 module Mhtml
+
   LINE_BREAK = "\r\n".freeze
   DOUBLE_LINE_BREAK = "#{LINE_BREAK}#{LINE_BREAK}".freeze
+
   STATUS_LINE = "HTTP/1.1 200 OK#{LINE_BREAK}".freeze
+
+  FILE_EXTENSIONS = [ '.mht', '.mhtml' ].freeze
+
+  def self.is_mhtml(filename)
+    ext = File.extname(filename)
+    return false if ext.empty?
+    ext.downcase!
+    FILE_EXTENSIONS.include?(ext)
+  end
 end
 
 require 'mhtml/document'
--- spec/mhtml_spec.rb
+++ spec/mhtml_spec.rb
@@ -4,4 +4,30 @@ RSpec.describe Mhtml do
   it 'has a version number' do
     expect(Mhtml::VERSION).not_to be nil
   end
+
+  describe '.is_mhtml' do
+    context 'filename has no extension' do
+      it 'returns false' do
+        expect(Mhtml::is_mhtml('p00ts')).to be false
+      end
+    end
+
+    context 'filename extension is .html' do
+      it 'returns false' do
+        expect(Mhtml::is_mhtml('w00ts.html')).to be false
+      end
+    end
+
+    context 'filename extension is .mht' do
+      it 'returns true' do
+        expect(Mhtml::is_mhtml('parp.parps.mht')).to be true
+      end
+    end
+
+    context 'filename extension is .mhtml' do
+      it 'returns true' do
+        expect(Mhtml::is_mhtml('parp.par_ps.mhtml')).to be true
+      end
+    end
+  end
 end
